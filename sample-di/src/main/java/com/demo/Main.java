0dabf5bed3d878d10bf634b87e148b8f0b30448e
hhtun137 <htunaunghlaing199@gmail.com>
2023-02-28 10:15:38 +0630

    spring core base

---
+++ sample-di/src/main/java/com/demo/Main.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+import com.demo.beans.Person;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.lang.reflect.AnnotatedArrayType;
+
+public class Main {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        context.registerShutdownHook();
+
+        Person person = context.getBean(Person.class);
+        person.setName("Thaw Thaw ");
+        System.out.println("Name:" + person.getName());
+    }
+}
---
+++ sample-di/src/main/java/com/demo/beans/Person.java
@@ -0,0 +1,17 @@
+package com.demo.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+}
